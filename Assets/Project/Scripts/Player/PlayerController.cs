4f793920ac4b50e7bac63a770ade179457d4f893
AlexRP <alexrubioplana14@gmail.com>
2024-10-02 19:03:34 +0200

    Player State Machine

--- Assets/Project/Scripts/Player/PlayerController.cs
+++ Assets/Project/Scripts/Player/PlayerController.cs
@@ -3,11 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+using UnityEngine.InputSystem.XInput;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Inputs"), SerializeField]
     private InputActionReference movementAction;
+
+    public enum State { IDLE, MOVING, KNOCKBACK, INVENCIBILITY, DEAD };
+
+    private State currentState;
 
     [SerializeField]
     private float speed;
@@ -42,8 +48,74 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Move();
-        Rotate();
+        switch (currentState)
+        {
+            case State.IDLE:
+                CheckIdleOrMovingState();
+                break;
+            case State.MOVING:
+                Move();
+                Rotate();
+                CheckIdleOrMovingState();
+                break;
+            case State.KNOCKBACK:
+                break;
+            case State.INVENCIBILITY:
+                break;
+            case State.DEAD:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void CheckIdleOrMovingState()
+    {
+        if(movementDirection == Vector2.zero && currentState == State.MOVING)
+        {
+            ChangeState(State.IDLE);
+        }
+        else if(movementDirection != Vector2.zero && currentState == State.IDLE)
+        {
+            ChangeState(State.MOVING);
+        }
+    }
+
+    public void ChangeState(State state)
+    {
+        switch (currentState)
+        {
+            case State.IDLE:
+                break;
+            case State.MOVING:
+               
+                break;
+            case State.KNOCKBACK:
+                break;
+            case State.INVENCIBILITY:
+                break;
+            case State.DEAD:
+                break;
+            default:
+                break;
+        }
+
+        switch (currentState)
+        {
+            case State.IDLE:
+                break;
+            case State.MOVING:
+                break;
+            case State.KNOCKBACK:
+                break;
+            case State.INVENCIBILITY:
+                break;
+            case State.DEAD:
+                break;
+            default:
+                break;
+        }
+        currentState = state;
     }
 
     private void Move()
@@ -79,8 +151,6 @@ public class PlayerController : MonoBehaviour
     {
         currentHealth -= damage;
 
-        Debug.Log(currentHealth);
-
         if(currentHealth < 0)
         {
             Die();
@@ -89,12 +159,11 @@ public class PlayerController : MonoBehaviour
 
     private void Die()
     {
-        PlayersManager.instance.ErasePlayer(gameObject);
-        Destroy(gameObject);
+        ChangeState(State.DEAD);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Enemy"))
+        if (collision.CompareTag("Enemy") && currentState != State.INVENCIBILITY)
         {
             ReceiveDamage(collision.GetComponent<Enemy>().GetDamage());
         }
