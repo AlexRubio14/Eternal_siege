e4f2d0c8a7892f1ee35a10872cb6b54e2abbdac5
AlexRP <alexrubioplana14@gmail.com>
2024-10-03 00:42:52 +0200

    Camera zoom almost done

--- Assets/Project/Scripts/Camera/CameraController.cs
+++ Assets/Project/Scripts/Camera/CameraController.cs
@@ -1,16 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]
-    private Camera camera;
+
+    [Header("Zoom Values")]
+    [SerializeField] private float minZoomValue;
+    [SerializeField] private float maxZoomValue;
+
+
+    [Header("Offsets")]
+    [SerializeField] private float offsetZoomOut;
+    [SerializeField] private float offsetZoomIn;
+    [SerializeField] private float zoomOutSpeed;
+
+
+    [SerializeField] private Camera camera;
 
     private float width = 0f;
     private float height = 0f;
 
-    //Camera positions references
     struct CameraReferences
     {
         public float xMin;
@@ -21,25 +32,17 @@ public class CameraController : MonoBehaviour
 
     private CameraReferences cameraReferences;
 
-    private float minZoomValue;
+    public enum CameraState {  ZOOM_OUT, ZOOM_IN, STOP  }
 
-    [SerializeField] private float maxZoomValue;
-
-    [SerializeField] private float offsetZoom;
-
-    [SerializeField] private float zoomOutSpeed;
+    [SerializeField] private CameraState cameraState;
 
 
     private void Awake()
     {
         minZoomValue = camera.orthographicSize;
+        cameraState = CameraState.STOP;
 
         CalculateCameraReferences();
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
@@ -48,27 +51,72 @@ public class CameraController : MonoBehaviour
         if (PlayersManager.instance.GetPlayersList().Count == 0)
             return;
 
-        if(CheckIfPlayersAreOutOfCamera())
+        CheckCameraState();
+
+        switch (cameraState)
         {
-            ZoomOut();
-        }
-        else
-        {
-            ZoomIn();
+            case CameraState.ZOOM_OUT:
+                ZoomOut();
+                break;
+            case CameraState.ZOOM_IN:
+                ZoomIn();
+                break;
+            case CameraState.STOP:
+                break;
+            default:
+                break;
         }
     }
 
-    private bool CheckIfPlayersAreOutOfCamera()
+
+    private void CheckCameraState()
     {
-        foreach(GameObject player in PlayersManager.instance.GetPlayersList())
+        foreach (GameObject player in PlayersManager.instance.GetPlayersList())
         {
-            if (player.transform.position.x < this.cameraReferences.xMin + offsetZoom || player.transform.position.x > this.cameraReferences.xMax - offsetZoom
-                || player.transform.position.y < this.cameraReferences.yMin + offsetZoom || player.transform.position.y > this.cameraReferences.yMax - offsetZoom)
+            if (CheckStopZoom(player))
             {
-                return true;
+                cameraState = CameraState.STOP;
+            }
+            else if (CheckZoomOut(player))
+            {
+                cameraState = CameraState.ZOOM_OUT;
+            }
+            else if (CheckZoomIn(player))
+            {
+                cameraState = CameraState.ZOOM_IN;
             }
         }
+    }
 
+    private bool CheckStopZoom(GameObject player)
+    {
+        if(player.transform.position.x > this.cameraReferences.xMin + offsetZoomOut && player.transform.position.x < this.cameraReferences.xMin + offsetZoomIn
+            || player.transform.position.x < this.cameraReferences.xMax - offsetZoomOut && player.transform.position.x > this.cameraReferences.xMax - offsetZoomIn
+            || player.transform.position.y > this.cameraReferences.yMin + offsetZoomOut && player.transform.position.y < this.cameraReferences.yMin + offsetZoomIn
+            || player.transform.position.y < this.cameraReferences.yMax - offsetZoomOut && player.transform.position.y > this.cameraReferences.yMax - offsetZoomIn)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool CheckZoomOut(GameObject player)
+    {
+        if (player.transform.position.x < this.cameraReferences.xMin + offsetZoomOut || player.transform.position.x > this.cameraReferences.xMax - offsetZoomOut
+                || player.transform.position.y < this.cameraReferences.yMin + offsetZoomOut || player.transform.position.y > this.cameraReferences.yMax - offsetZoomOut)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool CheckZoomIn(GameObject player)
+    {
+        if(player.transform.position.x > this.cameraReferences.xMin + offsetZoomIn || player.transform.position.x < this.cameraReferences.xMax - offsetZoomIn
+            || player.transform.position.y > this.cameraReferences.yMin + offsetZoomIn || player.transform.position.y < this.cameraReferences.yMax - offsetZoomIn)
+        {
+            return true;
+        }
         return false;
     }
 
@@ -88,10 +136,8 @@ public class CameraController : MonoBehaviour
         if(camera.orthographicSize <= maxZoomValue) 
         {
             camera.orthographicSize += zoomOutSpeed;
-
             CalculateCameraReferences();
         }
-
     }
 
     private void ZoomIn()
