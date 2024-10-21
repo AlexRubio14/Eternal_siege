1e0995dfc8e5f1d809cb731506f59682267f9353
Marcos <marcos.ruiz@enti.cat>
2024-10-21 22:17:21 +0200

    Merge fixed more

--- Assets/Project/Scripts/Player/ChooseUpgrade.cs
+++ Assets/Project/Scripts/Player/ChooseUpgrade.cs
@@ -8,30 +8,49 @@ using TMPro;
 public class ChooseUpgrade : MonoBehaviour
 {
     private List<GameObject> cards;
+    private List<float> moveTimer;
+    private List<bool> canMove;
     int index;
 
     private void Start()
-    {
-
+    { 
         index = 0;
+        moveTimer = new List<float>() { 0,0};
+        canMove = new List<bool>() { true, true};
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < moveTimer.Count; i++)
+        {
+            if (moveTimer[i] > 0.3f && !canMove[i])
+            {
+                canMove[i] = true;
+            }
+            moveTimer[i] += Time.deltaTime;
+        }
     }
     public void MoveRight(InputAction.CallbackContext obj)
     {
-        if(obj.started && index < cards.Count - 1) 
+        if(obj.started && index < cards.Count - 1 && canMove[0]) 
         {
             cards[index].GetComponent<Outline>().enabled = false;
             index++;
             cards[index].GetComponent<Outline>().enabled = true;
+            canMove[0] = false;
+            moveTimer[0] = 0;
         }
     }
 
     public void MoveLeft(InputAction.CallbackContext obj)
     {
-        if (obj.started && index > 0)
+        if (obj.started && index > 0 && canMove[1])
         {
             cards[index].GetComponent<Outline>().enabled = false;
             index--;
             cards[index].GetComponent<Outline>().enabled = true;
+            canMove[1] = false;
+            moveTimer[1] = 0;
         }
     }
 
